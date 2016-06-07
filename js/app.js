9b394cbbee06de86a60d2eda2853a69f59035249
James Thornbrue <jthornbrue@blastmotion.com>
2016-06-07 11:33:09 -0700

    add accelerometer chart

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ angular.module("app", [])
     
     function upload(file, result) {
     
+    	$scope.loaded = true;
+    
     	$scope.file = file;
     	
     	$scope.json = JSON.parse(result);
@@ -24,36 +26,79 @@ angular.module("app", [])
         });
         
         gyr = {
-            	timestamp: _.pluck(gyr, 'timestamp'),
-            	x: _.pluck(gyr, 'x'),
-            	y: _.pluck(gyr, 'y'),
-            	z: _.pluck(gyr, 'z')
+            timestamp: _.pluck(gyr, 'timestamp'),
+            x: _.pluck(gyr, 'x'),
+            y: _.pluck(gyr, 'y'),
+            z: _.pluck(gyr, 'z')
+        };
+            
+        var acc = _.map($scope.json.capture.calibratedSensorData.samples, function (sample) {
+            
+        	var xyz = _.map(sample[1].split(','),  parseFloat);
+            	
+        	return {
+            	timestamp: sample[0],
+            	x: xyz[0],
+            	y: xyz[1],
+            	z: xyz[2]
             };
+        });
         
-        $scope.gyr = gyr;
+        acc = {
+            timestamp: _.pluck(acc, 'timestamp'),
+            x: _.pluck(acc, 'x'),
+            y: _.pluck(acc, 'y'),
+            z: _.pluck(acc, 'z')
+        };
+        
+        $scope.acc = acc;
         
     	var data = [{
+        	x: acc.timestamp,
+        	y: acc.x,
+        	name: 'acc x',
+        	type: 'scatter'
+        }, {
+        	x: acc.timestamp,
+        	y: acc.y,
+        	name: 'acc y',
+        	type: 'scatter'
+        }, {
+        	x: acc.timestamp,
+        	y: acc.z,
+        	name: 'acc z',
+        	type: 'scatter'
+        }, {
         	x: gyr.timestamp,
         	y: gyr.x,
-        	name: 'x',
-        	type: 'scatter'
+        	name: 'gyr x',
+        	type: 'scatter',
+        	xaxis: 'x',
+        	yaxis: 'y2'
         }, {
         	x: gyr.timestamp,
         	y: gyr.y,
-        	name: 'y',
-        	type: 'scatter'
+        	name: 'gyr y',
+        	type: 'scatter',
+        	xaxis: 'x',
+        	yaxis: 'y2'
         }, {
         	x: gyr.timestamp,
         	y: gyr.z,
-        	name: 'z',
-        	type: 'scatter'
+        	name: 'gyr z',
+        	type: 'scatter',
+        	xaxis: 'x',
+        	yaxis: 'y2'
         }];
         
         var layout = {
-        	title: 'Calibrated Gyro'
+        	title: 'Calibrated Data',
+        	yaxis: {domain: [.55, 1], title: 'm/s<sup>2</sup>'},
+        	yaxis2: {domain: [0, .45], title:'deg/s'},
+        	xaxis: {anchor: 'y2'}
         };
         
-        Plotly.newPlot('cal-gyr', data, layout);
+        Plotly.newPlot('plotly', data, layout);
     }
 
     // setup drag/drop zone over the map
